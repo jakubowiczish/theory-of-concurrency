8b832dd6aa13a4aaa5b19f977179f209a641d198
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-10-21 00:00:04 +0200

    Small refactor.

--- lab3/src/main/java/BoundedBuffer.java
+++ lab3/src/main/java/BoundedBuffer.java
@@ -13,11 +13,15 @@ public class BoundedBuffer {
 
     public void put(Object x) throws InterruptedException {
         lock.lock();
+
         try {
             while (count == items.length)
                 notFull.await();
+
             items[putptr] = x;
-            if (++putptr == items.length) putptr = 0;
+            if (++putptr == items.length)
+                putptr = 0;
+
             ++count;
             notEmpty.signal();
         } finally {
@@ -27,11 +31,15 @@ public class BoundedBuffer {
 
     public Object take() throws InterruptedException {
         lock.lock();
+
         try {
             while (count == 0)
                 notEmpty.await();
+
             Object x = items[takeptr];
-            if (++takeptr == items.length) takeptr = 0;
+            if (++takeptr == items.length)
+                takeptr = 0;
+
             --count;
             notFull.signal();
             return x;
