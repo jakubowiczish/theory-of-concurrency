64a2a4f3ef2d9a06b40baaa85bed6e7170313603
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-11-02 22:09:42 +0100

    Add basic solution for lab4/task1.

--- lab3/src/main/java/Main.java
+++ lab3/src/main/java/Main.java
@@ -1,10 +1,12 @@
+import static task1.FirstTaskExecutioner.executeBoundedBufferOnProducersAndConsumers;
 import static task2.SecondTaskExecutioner.executePrintersMonitor;
+import static task3.ThirdTaskExecutioner.executeWaitersMonitor;
 
 public class Main {
 
     public static void main(String[] args) throws InterruptedException {
 //        executeBoundedBufferOnProducersAndConsumers();
-        executePrintersMonitor();
-//        executeWaitersMonitor();
+//        executePrintersMonitor();
+        executeWaitersMonitor();
     }
 }
