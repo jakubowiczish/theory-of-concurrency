37251e4249de7c4ee2a65ca8abff94c982bea7eb
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-11-24 20:01:23 +0100

    Update .gitignore file. Add solution to lab6/task1a.

---
+++ lab6/task1a.js
@@ -0,0 +1,37 @@
+function printAsync(s, cb) {
+    var delay = Math.floor((Math.random() * 1000) + 500);
+    setTimeout(function () {
+        console.log(s);
+        if (cb) cb();
+    }, delay);
+}
+
+function task1(cb) {
+    printAsync("1", function () {
+        task2(cb);
+    });
+}
+
+function task2(cb) {
+    printAsync("2", function () {
+        task3(cb);
+    });
+}
+
+function task3(cb) {
+    printAsync("3", cb);
+}
+
+function loop(n) {
+    if (n === 1) {
+        task1(function () {
+            console.log("done !");
+        });
+        return;
+    }
+    task1(function () {
+        loop(n - 1)
+    });
+}
+
+loop(3);
