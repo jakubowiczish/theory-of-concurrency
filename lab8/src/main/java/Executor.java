95537e219d5baf5bb14930a1ac8707755907f587
jakubowiczish <jakub77plotnikowski@gmail.com>
2019-12-16 19:33:32 +0100

    Add documentation for lab8 and java code in executor class.

--- lab8/src/main/java/Executor.java
+++ lab8/src/main/java/Executor.java
@@ -18,37 +18,33 @@ public class Executor extends Thread {
 
         Vertex s = new Vertex(null, null, "S");
 
-        P1 p1 = new P1(s, drawer);
-        this.runner.addThread(p1);
-
-        //start threads
+        P1 p1_1 = new P1(s, drawer);
+        this.runner.addThread(p1_1);
         this.runner.startAll();
 
-        //p2,p3
-        P2 p2 = new P2(p1.getObj(), drawer);
-        P3 p3 = new P3(p1.getObj().getRight(), drawer);
-        this.runner.addThread(p2);
-        this.runner.addThread(p3);
-
-        //start threads
+        P2 p2_2 = new P2(p1_1.getObj(), drawer);
+        P3 p3_2 = new P3(p1_1.getObj().getRight(), drawer);
+        this.runner.addThread(p2_2);
+        this.runner.addThread(p3_2);
         this.runner.startAll();
 
-        //p5^2,p6^2
-        P5 p5A = new P5(p2.getObj(), drawer);
-        P5 p5B = new P5(p3.getObj().getRight(), drawer);
-        P6 p6A = new P6(p2.getObj().getRight(), drawer);
-        P6 p6B = new P6(p3.getObj(), drawer);
-        this.runner.addThread(p5A);
-        this.runner.addThread(p5B);
-        this.runner.addThread(p6A);
-        this.runner.addThread(p6B);
-
-        //start threads
+        P5 p5a_3 = new P5(p2_2.getObj(), drawer);
+        P3 p3_3 = new P3(p3_2.getObj().getRight(), drawer);
+        P5 p6_3 = new P5(p3_2.getObj().getRight(), drawer);
+        this.runner.addThread(p5a_3);
+        this.runner.addThread(p3_3);
+        this.runner.addThread(p6_3);
         this.runner.startAll();
 
-        //done
-        System.out.println("done");
-        drawer.draw(p6B.getObj());
+        P6 p6_4 = new P6(p5a_3.getObj(), drawer);
+        P6 p5b_4 = new P6(p3_3.getObj().getRight(), drawer);
+        P6 p6v2_4 = new P6(p3_3.getObj(), drawer);
+        this.runner.addThread(p6_4);
+        this.runner.addThread(p5b_4);
+        this.runner.addThread(p6v2_4);
+        this.runner.startAll();
 
+        System.out.println("DONE!");
+        drawer.draw(p6v2_4.getObj());
     }
 }
